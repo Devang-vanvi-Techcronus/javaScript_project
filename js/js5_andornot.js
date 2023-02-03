cc570d5abf168be84d7ec297cf53148495917afa
Devang Vanvi <devang.vanvi@techcronus.com>
2023-02-03 18:48:05 +0530

    initial commit 2

---
+++ js/js5_andornot.js
@@ -0,0 +1,19 @@
+// Write an if condition to check that age is between 14 and 90 inclusively.
+
+// “Inclusively” means that age can reach the edges 14 or 90.
+
+
+let age = 25
+
+if(age<=14 && age>=90){
+    alert ('gsgws')
+}
+
+// Write an if condition to check that age is NOT between 14 and 90 inclusively.
+
+
+if(age<14 || age>90){
+    console.log("age below 18");
+}else{
+    console.log('age abov 18');
+}
