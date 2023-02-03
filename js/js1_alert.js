546b1c734e250e5df05f792d6d1515a65609fbd3
Devang Vanvi <devang.vanvi@techcronus.com>
2023-02-03 18:01:54 +0530

    initail commit

---
+++ js/js1_alert.js
@@ -0,0 +1,17 @@
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+alert(3 +
+    1
+    + 2);
+
+// -----------------------------------
+    alert("Hello")
+
+    [1, 2].forEach(alert);
+
+// -----------------------------------
+
+
+alert("Hello")[1, 2].forEach(alert);
---
+++ js/js3_variable.js
@@ -0,0 +1,44 @@
+// let message;
+
+// message = 'Hello!';
+
+// message = 'World!'; // value changed
+
+// alert(message);
+
+
+// result = prompt(title, [default]);
+
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3")
+// console.log("2" * "3")
+// console.log(4 + 5 + "px")
+// console.log("$" + 4 + 5)
+// console.log("4" - 2)
+// console.log("4px" - 2)
+// console.log("  -9  " + 5)
+// console.log("  -9  " - 5)
+// console.log(null + 1)
+// console.log(undefined + 1)
+// console.log(" \t \n" - 2)
+
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// console.log(a + b);
+// console.log(+a + +b);
+
+// alert( undefined === null )
+
+
+
+// console.log(5 > 4)
+// console.log("apple" > "pineapple")
+// console.log("2" > "12")
+// console.log(undefined == null)
+// console.log(undefined === null)
+// console.log(null == "\n0\n")
+// console.log(null === +"\n0\n")
+
