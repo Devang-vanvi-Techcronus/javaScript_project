5cd53f21bc5dbbe3efccbbdde25c9e81e7abff9a
Devang Vanvi <devang.vanvi@techcronus.com>
2023-02-06 18:17:53 +0530

    6 feb

---
+++ js/js7_switch.js
@@ -0,0 +1,106 @@
+// Write the code using if..else which would correspond to the following switch:
+
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+// let browser = prompt('enter browser name','')
+
+// if(browser ==  'edge'){
+//     alert( "You've got the Edge!" );
+// }
+// else if (browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera' ){
+//     alert( 'Okay we support these browsers too' );
+// }
+// else{
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+
+
+// Rewrite the code below using a single switch statement:
+
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+
+// switch (a) {
+//     case (0): alert( 0 )
+        
+//         break;
+//         case (1): alert( 1 )
+        
+//         break;
+//         case (2): alert( 2 )
+        
+//         break;
+//         case (2 ||  3): alert( '12' )
+        
+//         break;
+
+
+
+
+//     default:
+//         break;
+// }
+
+
+// function min(num ,num1){
+//     if(num<num1){
+//         console.log(num);
+//     }
+//     else{
+//         console.log(num1);
+//     }
+    
+// }
+// min(2, 5) 
+
+
+// pow(3, 2) = 3 * 3 = 9
+
+
+function pow(a,b){
+    let data = a*(a**b)
+    console.log(data);
+}
+pow(1, 100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
